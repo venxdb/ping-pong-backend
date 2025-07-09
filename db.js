8027e69e2513464f6c9fec8185bfd75cc29a372a
venxdb <micheleventurini95@gmail.com>
2025-07-09 17:42:18 +0200

    db.js

--- db.js
+++ db.js
@@ -2,12 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
-
